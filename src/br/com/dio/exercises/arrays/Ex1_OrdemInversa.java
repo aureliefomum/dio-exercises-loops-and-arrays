ea56eb97829b8d169329678db7ab0d6043e0c606
aureliefomum <azam_aurelie@yahoo.com>
2022-01-15 12:49:39 -0300

    Arrays-Consoantes

--- src/br/com/dio/exercises/arrays/Ex1_OrdemInversa.java
+++ src/br/com/dio/exercises/arrays/Ex1_OrdemInversa.java
@@ -5,7 +5,7 @@ e mostre-os na ordem inversa.
 */
 public class Ex1_OrdemInversa {
     public static void main(String[] args) {
-        //existem 5 posições aqui dentro
+        //NB:existem 5 posições aqui dentro
         int[] vetor = {-5,-6,15,50,8,4};
         //System.out.println(vetor.length);
         System.out.println("Vetor: ");
