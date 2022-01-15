fd2a31a02487364545eec494676c55c1b2adffdd
aureliefomum <azam_aurelie@yahoo.com>
2022-01-15 12:47:54 -0300

    Arrays-Consoantes

--- src/br/com/dio/exercises/arrays/Ex1_OrdemInversa.java
+++ src/br/com/dio/exercises/arrays/Ex1_OrdemInversa.java
@@ -1,4 +1,27 @@
 package br.com.dio.exercises.arrays;
-
+/*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
 public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+        //existem 5 posições aqui dentro
+        int[] vetor = {-5,-6,15,50,8,4};
+        //System.out.println(vetor.length);
+        System.out.println("Vetor: ");
+        int count = 0;
+
+      while(count < (vetor.length)){
+          System.out.print(vetor[count] + " ");
+          count++;
+      }
+        System.out.println("\nVetorInverso: ");
+      for(int i = (vetor.length-1);i>=0; i--){
+            System.out.print(vetor[i] + " ");
+      }
+
+
+
+    }
+
 }
